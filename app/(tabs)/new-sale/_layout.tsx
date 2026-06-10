f47866024a608d1114fe16476d390f589102ef5f
AfricanBudu <abdullahagboola202@gmail.com>
2026-06-10 12:50:27 +0100

    Wrap NewSaleLayout in CartProvider to manage cart state and update screen options for improved navigation and user experience.

--- app/(tabs)/new-sale/_layout.tsx
+++ app/(tabs)/new-sale/_layout.tsx
@@ -1,10 +1,18 @@
+import { CartProvider } from "@/context/CartContext";
 import { Stack } from "expo-router";
 
 export default function NewSaleLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="cart-review" options={{ headerShown: false }} />
-    </Stack>
+    <CartProvider>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "New Sale",
+          }}
+        />
+        <Stack.Screen name="cart-review" options={{}} />
+      </Stack>
+    </CartProvider>
   );
 }
