ae726c240d72a6593806f1d8dd61ccc4dcae7b64
AfricanBudu <abdullahagboola202@gmail.com>
2026-06-17 16:16:55 +0100

    Enhance TextMono component by adding additional styling classes for improved typography, including text color, size, tracking, and leading adjustments.

--- components/typography/TextMono.tsx
+++ components/typography/TextMono.tsx
@@ -1,5 +1,10 @@
 import { Text as RNText, TextProps } from "react-native";
 
 export function TextMono({ className, ...props }: TextProps) {
-  return <RNText className={`font-mono ${className ?? ""}`} {...props} />;
+  return (
+    <RNText
+      className={`font-mono text-black text-base tracking-tight leading-none ${className ?? ""}`}
+      {...props}
+    />
+  );
 }
